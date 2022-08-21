2684b7b264bf075dffe76b2e04fbc266ffb1b199
zcchen <Bill.Chen.fs.cn@outlook.com>
2022-08-21 23:33:37 +0800

    1st commit.

---
+++ vector3.lua
@@ -0,0 +1,158 @@
+-- vector3
+-- CHEN, Zhechuan
+-- https://github.com/zcchen/vector-lua.git
+-- MIT License
+
+local calc = {}
+
+local vec3, quat
+
+vec3 = {
+    __add = function(v, u) return v:add(u) end,
+    __sub = function(v, u) return v:sub(u) end,
+    __mul = function(v, u)
+        if type(u) == "number" then
+            return v:scale(u)
+        elseif calc.isvec3(u) then
+            return v:dot(u)
+        else
+            error("vec3 can only scale with number or dot-multiply with vec3 at operator mul.")
+        end
+    end,
+    __div = function(v, u)
+        if type(u) == "number" then
+            return v:scale(1 / u)
+        else
+            error("vec3 can only be divided by a number.")
+        end
+    end,
+    __len = function(v) return v:length() end,
+    __unm = function(v) return v:scale(-1) end,
+    __tostring = function(v) return string.format("(%f, %f, %f)", v.x, v.y, v.z) end,
+
+    __call = function(_, x, y, z)
+        return setmetatable({
+            x = x or 0,
+            y = y or 0,
+            z = z or 0,
+        }, vec3)
+    end,
+
+    __index = {
+        unpack = function(v)
+            return v.x, v.y, v.z
+        end,
+
+        set = function(v, x, y, z)
+            v.x = x
+            v.y = y
+            v.z = z
+            return v
+        end,
+
+        add = function(v, u)
+            if not calc.isvec3(u) then error('vec3 can only be added by another vec3.') end
+            local out = vec3()
+            out.x = v.x + u.x
+            out.y = v.y + u.y
+            out.z = v.z + u.z
+            return out
+        end,
+
+        sub = function(v, u)
+            if not calc.isvec3(u) then error('vec3 can only be sub by another vec3.') end
+            local out = vec3()
+            out.x = v.x - u.x
+            out.y = v.y - u.y
+            out.z = v.z - u.z
+            return out
+        end,
+
+        dot = function(v, u)
+            if not calc.isvec3(u) then
+                error('vec3 can only be dot multiplied by another vec3.')
+            end
+            return v.x * u.x + v.y * u.y + v.z * u.z
+        end,
+
+        cross = function(v, u)
+            if not calc.isvec3(u) then
+                error('vec3 can only be cross multiplied by another vec3.')
+            end
+            local out = vec3()
+            out.x = v.y * u.z - v.z * u.y
+            out.y = v.z * u.x - v.x * u.z
+            out.z = v.x * u.y - v.y * u.x
+            return out
+        end,
+
+        scale = function(v, s)
+            if type(s) ~= "number" then
+                error("vec3 can only be scaled by a number.")
+            end
+            local out = vec3()
+            out.x = v.x * s
+            out.y = v.y * s
+            out.z = v.z * s
+            return out
+        end,
+
+        length = function(v)
+            return math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
+        end,
+
+        norm = function(v)
+            local len = v:length()
+            if len == 0 then
+                return vec3(v.x, v.y, v.z)
+            else
+                return v:scale(1 / len)
+            end
+        end,
+
+        distance = function(v, u)
+            if not calc.isvec3(u) then
+                error('distance can ONLY be calculated by vec3s.')
+            end
+            return v:sub(v, u):length()
+        end,
+
+        angle = function(v, u)
+            if not calc.isvec3(u) then
+                error('angle can ONLY be calculated by vec3s.')
+            end
+            return math.acos(v:dot(u) / (v:length() + u:length()))
+        end,
+
+        project = function(v, u)
+            if not calc.isvec3(u) then
+                error('The projected target must be another vec3..')
+            end
+            local out = vec3()
+            local unorm = u:norm()
+            local dot = v:dot(unorm)
+            out.x = unorm.x * dot
+            out.y = unorm.y * dot
+            out.z = unorm.z * dot
+            return out
+        end,
+    },
+}
+setmetatable(vec3, vec3)
+
+function calc.vec3(x, y, z)
+    return vec3(x, y, z)
+end
+function calc.isvec3(v)
+    return getmetatable(v) == vec3
+end
+function calc.vector(x, y, z)
+    return vec3(x, y, z)
+end
+function calc.isvector(v)
+    return getmetatable(v) == vec3
+end
+
+-- TODO: add quaternions object
+
+return calc
